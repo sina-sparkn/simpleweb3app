cb601d0b4b99585bd5d85e4e12c826cafd341d79
ssparknt <sina.hakim.red@gmail.com>
2022-12-06 01:10:07 +0200

    deployd the smartcontract to goerli blockchain

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,4 @@
+// *contract address : 0x58AB0e6c396071c5bf42496F8D0A341EAaCd520e
 const main = async () => {
   const [deployer] = await hre.ethers.getSigners();
   const accountBalance = await deployer.getBalance();
